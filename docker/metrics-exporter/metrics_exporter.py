549e7422a58dde2a92e64258695639a772e357a0
ilconte96 <marchese.angelo1@gmail.com>
2020-01-08 09:55:55 +0100

    Update metrics_exporter.py
    Changed upstream_response_time log in request_time log

--- docker/metrics-exporter/metrics_exporter.py
+++ docker/metrics-exporter/metrics_exporter.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.DEBUG)
 total_request_count = 0
 last_request_length = 0
 last_body_bytes_sent = 0
-last_upstream_response_time = 0.0
+last_request_time = 0.0
 
 
 endpoint_request_count = {}
@@ -21,7 +21,7 @@ def job():
     global total_request_count
     global last_request_length
     global last_body_bytes_sent
-    global last_upstream_response_time
+    global last_request_time
     global endpoint_request_count
     f = subprocess.Popen(['tail','-F','/app/logs/access.log'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
     p = select.poll()
@@ -36,7 +36,7 @@ def job():
                 endpoint_request_count[tokens[1]]+=1
             last_request_length = int(tokens[2])
             last_body_bytes_sent = int(tokens[3])
-            last_upstream_response_time = float(tokens[4])
+            last_request_time = float(tokens[4])
             total_request_count += 1
 
 @app.before_first_request
@@ -54,7 +54,7 @@ def get_metrics():
     response = f"total_request_count {total_request_count}\n"
     response += f"last_request_length {last_request_length}\n"
     response += f"last_body_bytes_sent {last_body_bytes_sent}\n"
-    response += f"last_upstream_response_time {last_upstream_response_time}\n"
+    response += f"last_request_time {last_request_time}\n"
     for key,value in endpoint_request_count.items():
         response += f'endpoint_request_count{{endpoint="{key}"}} {value}\n'
     return response

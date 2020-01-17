bcda7e5a3d1a30d8195636583f21af71c66876c4
ilconte96 <marchese.angelo1@gmail.com>
2020-01-17 12:35:42 +0100

    Update metrics exporter
    added job that monitors error logs

--- docker/metrics-exporter/metrics_exporter.py
+++ docker/metrics-exporter/metrics_exporter.py
@@ -12,12 +12,21 @@ total_request_count = 0
 last_request_length = 0
 last_body_bytes_sent = 0
 last_request_time = 0.0
-
+total_errors = 0
 
 endpoint_request_count = {}
 
+def error_logs_job():
+    global total_errors
+    f = subprocess.Popen(['tail','-F','/app/logs/access.log'],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    p = select.poll()
+    p.register(f.stdout)
+    while True:
+        if p.poll(1):
+            f.stdout.readline()
+            total_errors += 1
 
-def job():
+def access_logs_job():
     global total_request_count
     global last_request_length
     global last_body_bytes_sent
@@ -40,10 +49,13 @@ def job():
             total_request_count += 1
 
 @app.before_first_request
-def start_job():
-    task = threading.Thread(target=job)
+def start_jobs():
+    task = threading.Thread(target=access_logs_job)
     task.start()
-    app.logger.info(f'Thread started')
+    app.logger.info(f'Access logs thread started')
+    task = threading.Thread(target=error_logs_job)
+    task.start()
+    app.logger.info(f'Error logs thread started')
     
 @app.route('/', methods=['GET'])
 def ping():
@@ -55,6 +67,7 @@ def get_metrics():
     response += f"last_request_length {last_request_length}\n"
     response += f"last_body_bytes_sent {last_body_bytes_sent}\n"
     response += f"last_request_time {last_request_time}\n"
+    response += f"total_errors {total_errors}\n"
     for key,value in endpoint_request_count.items():
         response += f'endpoint_request_count{{endpoint="{key}"}} {value}\n'
     return response
